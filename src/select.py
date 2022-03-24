00ca9ee310c4ef4baeddc81b3e0d925cd10d311c
Gentil-N <ngentil77@gmail.com>
2022-03-24 15:26:54 +0100

    selection updated

--- src/select.py
+++ src/select.py
@@ -3,23 +3,80 @@ import matplotlib.image as mpimg
 import numpy as np
 from matplotlib.widgets  import SpanSelector
 from matplotlib.widgets  import Button
+import tkinter as tk
+from tkinter import filedialog as fd
 
-img = mpimg.imread('./res/test.webp')
+WINDOW_TITLE = 'Extractor'
+IMG = np.array([])
+VMIN = 0
+VMAX = 0
+SELECTION = []
 
-#xdata = np.linspace(0,9*np.pi, num=301)
-#ydata = np.sin(xdata)
+def choose_file():
+    global IMG
+    global LABEL
+    global SELECTION
+    filename = fd.askopenfilename()
+    if filename != () and filename != "":
+        IMG = mpimg.imread(filename)
+        LABEL.config(text = filename)
+        SELECTION.clear()
+        print("Selected file: ", filename)
 
-fig, ax = plt.subplots()
-plt.imshow(img)
-#line, = ax.plot(xdata, ydata)
+def select_areas():
+    global IMG
+    if IMG.size == 0:
+        print("Unable to select areas! Choose a file first.")
+        return
+    fig, ax = plt.subplots()
+    plt.imshow(IMG)
 
-def onselect(vmin, vmax):
-    print(vmin, vmax)
+    def on_select(vmin, vmax):
+        global VMIN
+        global VMAX
+        VMIN = round(vmin)
+        VMAX = round(vmax)
+    def add_selection(event):
+        global SELECTION
+        SELECTION.append((VMIN, VMAX))
+        print("Selection added: (", VMIN, ",", VMAX, ")")
 
-rs = SpanSelector(ax, onselect, 'vertical', props=dict(facecolor='grey', alpha=0.35), interactive=True)
-axcut = plt.axes([0.9, 0.0, 0.1, 0.075])
-bcut = Button(axcut, 'Extract', color='grey', hovercolor='cyan')
-bcut.on_clicked(exctract)
+    rs = SpanSelector(ax, on_select, 'vertical', props=dict(facecolor='red', alpha=0.35), interactive=True)
+    axeselect = plt.axes([0.8, 0.0, 0.2, 0.075])
+    bselect = Button(axeselect, 'Add Selection')
+    bselect.on_clicked(add_selection)
+    plt.show()
 
-plt.show()
+def extract():
+    global SELECTION
+    global IMG
+    if len(SELECTION) == 0:
+        print("No selection.")
+        return
+    if IMG.size == 0:
+        print("Unable to extract! Choose a file first.")
+        return
+    for v in SELECTION:
+        print("Extraction of selection", v, "...", end='', flush=True)
+        data = []
+        offset = v[1] - v[0]
+        for i in range(len(IMG[0])):
+            average = 0
+            for j in range(offset):
+                average += (int(IMG[v[0] + j][i][0]) + int(IMG[v[0] + j][i][1]) + int(IMG[v[0] + j][i][2])) / 3
+            average /= offset
+            data.append(average)
+        print(" done")
+
+
+root = tk.Tk(className=WINDOW_TITLE)
+root.wm_title(WINDOW_TITLE)
+root.geometry('600x150+50+50')
+LABEL = tk.Label(root, text='No file selected')
+LABEL.pack()
+tk.Button(root, text='Choose File', command=choose_file, justify=tk.CENTER).pack()
+tk.Button(root, text='Select Areas', command=select_areas, justify=tk.CENTER).pack()
+tk.Button(root, text='Extract', command=extract, justify=tk.CENTER).pack()
+root.mainloop()
+
 print("haha")
